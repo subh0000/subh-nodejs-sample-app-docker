7ec02fc5f89ade872d790daefbfc117d3470be57
subh0000 <57227069+subh0000@users.noreply.github.com>
2021-01-08 13:36:29 +1100

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 'use strict';
 
-const request = require("request");
 const express = require('express');
 
 // Constants
-const PORT = process.env.PORT || 8080;
-const HOST = process.env.HOST || 'localhost';
+const PORT = 8080;
+const HOST = '0.0.0.0';
 
 // App
 const app = express();
-
 app.get('/', (req, res) => {
-  return res.send(`Hello World`);
+  res.send('Hello World');
 });
 
-app.listen(PORT);
-console.log(`Running on port :${PORT}`);
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
